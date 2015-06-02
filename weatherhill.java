ba9f62964ad4c8a3dd0cd0878c1d505ba96f779c
Michael Huang <changlin.h97@gmail.com>
2015-06-02 17:31:45 -0400

    weatherhill.java
    Main class started?

---
+++ weatherhill.java
@@ -0,0 +1,16 @@
+/*
+Michael Huang and Daniy Lomeo
+AP CS
+6.2.15
+This is the main class of the Text Based Game Weatherill
+*/
+
+import java.util.*;
+
+public class weatherhill
+{
+	public static void main (String[]args)
+	{
+		Scanner keyboard= new Scanner (System.in);
+	}
+}
