8f9cafbc4ad618e202e8aecfe5168c011544616f
Michael Huang <changlin.h97@gmail.com>
2015-06-02 23:22:23 -0400

    Changes galore
    asd

--- start.java
+++ start.java
@@ -1,4 +1,13 @@
 import java.util.*;
 
-public class start
+public class start extends weatherill
 {
+	Scanner keyboard = new Scanner (System.in);
+	System.out.println("Hi, enter your name");
+
+
+	String name = keyboard.nextLine();
+	Stat guy = new Stat(name);
+
+
+}
