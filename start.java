007f82d18aeeee4543ed1d30d55d931080fbf7bc
Michael Huang <changlin.h97@gmail.com>
2015-06-02 23:25:15 -0400

    yes
    oui

--- start.java
+++ start.java
@@ -2,12 +2,19 @@ import java.util.*;
 
 public class start extends weatherill
 {
+
 	Scanner keyboard = new Scanner (System.in);
-	System.out.println("Hi, enter your name");
+	public void startGame()
+	{
+		System.out.println("Hi, enter your name");
 
 
-	String name = keyboard.nextLine();
-	Stat guy = new Stat(name);
+		String name = keyboard.nextLine();
+		Stat guy = new Stat(name);
+	}
+
+
+
 
 
 }
